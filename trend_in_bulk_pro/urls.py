889afbc84ec41fd0c5da5c5095fc5c570d885998
sirajul musthafa <sirajulmusthafa786@gmail.com>
2026-07-22 18:17:13 +0530

    updated

--- trend_in_bulk_pro/urls.py
+++ trend_in_bulk_pro/urls.py
@@ -1,14 +1,46 @@
+# import os
+
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.contrib.sitemaps.views import sitemap
+# from django.http import HttpResponse
+# from django.urls import include, path
+
+# from trend_in_bulk_app.sitemap import StaticViewSitemap
+
+# handler404 = "trend_in_bulk_app.views.page_not_found"
+
+# sitemaps = {
+#     "static": StaticViewSitemap,
+# }
+
+
+# def robots_txt(request):
+#     file_path = os.path.join(settings.BASE_DIR, "trend_in_bulk_pro", "robots.txt")
+#     with open(file_path, "r") as file:
+#         return HttpResponse(file.read(), content_type="text/plain")
+
+
+# urlpatterns = [
+#     path("", include("trend_in_bulk_app.urls")),
+#     path("robots.txt", robots_txt),
+#     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
+# ]
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 import os
 
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from django.http import HttpResponse
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 from trend_in_bulk_app.sitemap import StaticViewSitemap
 
+
 handler404 = "trend_in_bulk_app.views.page_not_found"
+
 
 sitemaps = {
     "static": StaticViewSitemap,
@@ -16,14 +48,38 @@ sitemaps = {
 
 
 def robots_txt(request):
-    file_path = os.path.join(settings.BASE_DIR, "trend_in_bulk_pro", "robots.txt")
+    file_path = os.path.join(
+        settings.BASE_DIR,
+        "trend_in_bulk_pro",
+        "robots.txt",
+    )
+
     with open(file_path, "r") as file:
-        return HttpResponse(file.read(), content_type="text/plain")
+        return HttpResponse(
+            file.read(),
+            content_type="text/plain",
+        )
 
 
 urlpatterns = [
     path("", include("trend_in_bulk_app.urls")),
     path("robots.txt", robots_txt),
-    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="sitemap",
+    ),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# Temporary media serving on Render
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {
+            "document_root": settings.MEDIA_ROOT,
+        },
+    ),
+]
